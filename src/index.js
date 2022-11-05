7bac3ed6963a55543da3c15794c24343a08ddab7
gabriela.cattin <gabriela.cattin@totvs.com.br>
2022-11-05 14:13:22 -0300

    Apostila 1 de Programação Mobile

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
+const App = () => {
+    const estilosBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8, 
+        backgroundColor: 'blueviolet', color: 'white', border: 'none', width: "100%", 
+        borderRadius: 8};
+    const textoRotulo = "Nome:";
+    const textoBotao = () => {
+        return "Enviar";
+    }
+    return (
+        <div style={{margin:'auto', width:'768', backgroundColor: '#EEE', padding: 12, borderRadius: 8}}>
+            <label htmlFor="nome" style={{display: 'block', marginBottom: 4}}>{textoRotulo}</label>
+            <input type="text" id="nome" style={{paddingTop: 8, paddingBottom: 8, borderStyle: 'hidden',width: '100%', borderRadius: 8, outline: 'none', boxSizing: 'border-box'}}/>
+            <button style={estilosBotao}>{textoBotao()}</button>
+
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App/>,
+    document.querySelector("#root")
+)
